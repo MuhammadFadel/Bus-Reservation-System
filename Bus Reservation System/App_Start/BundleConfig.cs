8c3498cf85e25cf29cd29cc8a5ac56e962cc02c0
Muhammad Fadel <MuhammadFadel@fci.helwan.edu.eg>
2018-04-06 03:46:43 +0200

    We Added a Home View With new Style and scripts

--- Bus Reservation System/App_Start/BundleConfig.cs
+++ Bus Reservation System/App_Start/BundleConfig.cs
@@ -26,6 +26,45 @@ namespace Bus_Reservation_System
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+
+            //"~/Content/Assets/web/assets/jquery/jquery.min.js",
+            bundles.Add(new ScriptBundle("~/bundles/TempScript").Include(
+                      "~/Content/Assets/bootstrap/js/bootstrap.min.js",
+                      "~/Scripts/respond.js",
+                      "~/Content/Assets/popper/popper.min.js",
+                      "~/Content/Assets/tether/tether.min.js",
+                      "~/Content/Assets/smoothscroll/smooth-scroll.js",
+                      "~/Content/Assets/dropdown/js/script.min.js",
+                      "~/Content/Assets/touchswipe/jquery.touch-swipe.min.js",
+                      "~/Content/Assets/touchswipe/jquery.touch-swipe.min.js",
+                      "~/Content/Assets/parallax/jarallax.min.js",
+                      "~/Content/Assets/masonry/masonry.pkgd.min.js",
+                      "~/Content/Assets/imagesloaded/imagesloaded.pkgd.min.js",
+                      "~/Content/Assets/sociallikes/social-likes.js",
+                      "~/Content/Assets/vimeoplayer/jquery.mb.vimeo_player.js",
+                      "~/Content/Assets/mbr-testimonials-slider/mbr-testimonials-slider.js",
+                      "~/Content/Assets/bootstrapcarouselswipe/bootstrap-carousel-swipe.js",
+                      "~/Content/Assets/theme/js/script.js",
+                      "~/Content/Assets/slidervideo/script.js",
+                      "~/Content/Assets/gallery/player.min.js",
+                      "~/Content/Assets/formoid/formoid.min.js"));
+
+
+
+            bundles.Add(new StyleBundle("~/Content/TempStyle").Include(
+                      "~/Content/Assets/bootstrap/css/bootstrap.min.css",
+                      "~/Content/Assets/web/assets/mobirise-icons/mobirise-icons.css",
+                      "~/Content/Assets/tether/tether.min.css",
+                      "~/Content/Assets/bootstrap/css/bootstrap-grid.min.css",
+                      "~/Content/Assets/bootstrap/css/bootstrap-reboot.min.css",
+                      "~/Content/Assets/dropdown/css/style.css",
+                      "~/Content/Assets/socicon/css/styles.css",
+                      "~/Content/Assets/socicon/css/styles.css",
+                      "~/Content/Assets/theme/css/style.css",
+                      "~/Content/Assets/gallery/style.css",
+                      "~/Content/Assets/mobirise/css/mbr-additional.css"));
+
         }
     }
 }
